18593cafbca2033b1ee5a156fcb9e08ac03ae7b4
최은별 <99008137+dmsquf03@users.noreply.github.com>
2025-02-14 18:16:11 +0900

    Add: GemData에 실종류 추가
    - 실 이름에 사용할 yarnType 추가
    - ScriptableObject GemData에 작성
    - 푹신 / 동글 / 리본

--- StarWeaver/Assets/Scripts/Data/Items/GemData.cs
+++ StarWeaver/Assets/Scripts/Data/Items/GemData.cs
@@ -6,6 +6,7 @@ public class GemObjectData
   public int index;                   // 젬 인덱스
   public Sprite sprite;               // 젬 이미지
   public string name;                 // 젬 이름
+  public string yarnType;            // 해당 젬으로 만들어지는 실 종류류
 }
 
 [CreateAssetMenu(fileName = "GemData", menuName = "ScriptableObjects/GemData")]
